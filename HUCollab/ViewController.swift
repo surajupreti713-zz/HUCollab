1b822acee971218f0fb6251d77f2fed9d59cb5d6
Suraj Upreti <suraj@wheeapp-inc.com>
2019-03-28 16:20:54 -0400

    adding project files

---
+++ HUCollab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HUCollab
+//
+//  Created by Suraj Upreti on 3/28/19.
+//  Copyright © 2019 Suraj Upreti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
