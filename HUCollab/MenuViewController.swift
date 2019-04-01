423255697e70166d3b84201a58098a70263f1b0b
Suraj Upreti <suraj@wheeapp-inc.com>
2019-04-01 09:40:12 -0400

    added hamburger icon and implemented slide in transition functionality

---
+++ HUCollab/MenuViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MenuViewController.swift
+//  HUCollab
+//
+//  Created by Suraj Upreti on 3/31/19.
+//  Copyright © 2019 Suraj Upreti. All rights reserved.
+//
+
+import UIKit
+
+enum MenuType: Int {
+    case home
+    case profile
+    case signout
+}
+
+class MenuViewController: UITableViewController {
+    
+    var didTapMenuType: ((MenuType) -> Void)?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let menuType = MenuType(rawValue: indexPath.row) else { return }
+        dismiss(animated: true) { [weak self] in
+            print("Dismissing: \(menuType)")
+            self?.didTapMenuType?(menuType)
+        }
+    }
+    
+}
